47074f11cb0aca19c6502555be3df4ce9f9e2a2a
cogniteq <maksim.genyush@cogniteq.com>
2021-06-28 10:48:33 +0300

    Fixed modal on home page, did 16:9 for video block

--- src/components/UI/Player/Player.jsx
+++ src/components/UI/Player/Player.jsx
@@ -6,9 +6,7 @@ import './Player.scss'
 export default function Player({ url }) {
   return (
     <div className="player-container">
-      <div className="player-content">
         <ReactPlayer controls url={url} width="100%" height="100%" />
-      </div>
     </div>
   )
 }
